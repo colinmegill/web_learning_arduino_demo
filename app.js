8d26768e55bc0dae210f18acdc5c32979d4a2e3b
Colin Megill <colinmegill@gmail.com>
2013-12-24 13:31:51 -0800

    working typing on lcd with sockets on localhost

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
-var path = require("path"),
-    express = require("express");
-
+var express = require('express');
+var io = require('socket.io');
 var app = express()
-            .use(express.static(__dirname));
+  , server = require('http').createServer(app)
+  , io = io.listen(server);
+var five = require("johnny-five"),
+    board, lcd;
 
+server.listen(8080);
 
+app.use(express.static(__dirname));
 
-var port = process.env.PORT || 3000;
-app.listen(port);
-console.log("Started server on port " + port);
+// app.get('/', function(req, res){
+// 	res.sendfile(__dirname + '/index.html')
+// })
+
+/* begin robot */
+
+board = new five.Board();
+
+board.on("ready", function() {
+
+  lcd = new five.LCD({
+    // LCD pin name  RS  EN  DB4 DB5 DB6 DB7
+    // Arduino pin # 7    8   9   10  11  12
+    pins: [ 7, 8, 9, 10, 11, 12 ],
+  });
+
+  lcd.on("ready", function() {
+    io.sockets.on('connection', function (socket){
+  		socket.on('typing', function (data) {
+  		  lcd.clear().print(data.toString());
+  		  console.log(data)
+  		});    	
+    })
+  });
+
+  this.repl.inject({
+    lcd: lcd
+  });
+
+});
