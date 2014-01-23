ea7b02a23ff18cfe0b521d3c632093162b54bda5
terkkila <timo.erkkila@gmail.com>
2014-01-23 15:45:38 -0800

    Deleted a bunch of unused stuff, added comments to the main routine, and updated the commandline arguments.

--- qlearner.py
+++ qlearner.py
@@ -6,9 +6,24 @@ import io
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--relDistanceTh",type=float,default=0.05)
-parser.add_argument("--learnRate",type=float,default=0.3)
-parser.add_argument("--replayMemorySize",type=int,default=100)
+
+parser.add_argument("--relDistanceTh",
+                    metavar = '[0..1]',
+                    type = float,
+                    default = 0.05,
+                    help = 'Relative distance, how close should we get to the goal.')
+
+parser.add_argument("--learnRate",
+                    metavar = '[0..1]',
+                    type = float,
+                    default = 0.3,
+                    help = 'How much weight is given to the reward obtained by the new training sequence.')
+
+parser.add_argument("--replayMemorySize",
+                    metavar = '[1..n]',
+                    type = int,
+                    default = 100,
+                    help = 'How long training sequences can we hold at most.')
 
 args = parser.parse_args()
 
@@ -28,21 +43,38 @@ for line in inStream:
     # Read one line of input
     ID,currState,relDistance = io.parseLine(line)
 
+    # If the state is close enough to the target...
     if relDistance < args.relDistanceTh:
 
-        qlib.updateValue(value, replayMemory, args.learnRate, log = log)
+        # We can update the value function based on the replay memory
+        value.update(replayMemory, args.learnRate, log = log)
+
+        # And send a reset action back to the controller
         outStream.write('RESET_ACTION\n')
+
+        # Finish things up by erasing the replay memory, so that the new episode can start
         replayMemory = []
 
     else:
-        
+
+        # If we are not yet close enough to the goal, sample new action
         action = qlib.getNextAction(value,currState,actions)
+
+        # Update replay memory
         replayMemory.append( (currState,action) )
 
+        # If the maximum replay memory size is not yet reached...
         if len(replayMemory) < args.replayMemorySize:
+
+            # Send the delta action to the controller
             outStream.write(' '.join(map(str,['DELTA_ACTION',action[0],action[1]])) + '\n')
+
         else:
+
+            # Otherwise send a reset action to the controller
             outStream.write('RESET_ACTION\n')
+
+            # And erase replay memory, because we'll start a new episode
             replayMemory = []
 
 
--- qlib.py
+++ qlib.py
@@ -2,27 +2,9 @@ import copy
 import numpy as np
 from numpy.random import random_sample
 
-def getNextState(state,idx,delta):
-    nextState = copy.deepcopy(state)
-
-    if 0 < nextState[idx] + delta < 255:
-        nextState[idx] += delta
-    return nextState
-
-def relativeDistanceToTarget(state,targetSum):
-    return 1.0 * np.abs( sum(state) - targetSum ) / targetSum
-    
-def isCloseToTarget(state,targetSum,th):
-
-    if relativeDistanceToTarget(state,targetSum) < th:
-        return True
-    else:
-        return False
-
 def drawFromDistribution(values, probabilities):
     bins = np.add.accumulate(probabilities)
     return values[np.digitize(random_sample(1), bins)]
-
     
 def encode(state,action):
     return str(state) + ' + ' + str(action)
@@ -39,44 +21,6 @@ def getNextAction(value,state,actions):
 
     return nextAction
 
-
-class ControllerApplication(object):
-
-    def __init__(self):
-
-        # What target sum to reach
-        self.targetSum = 200
-
-        # What are the delta increments for the LEDs
-        self.deltas = [1, 10, 50, 20]
-
-        # How close to the true sum is close enough (in percentage)
-        self.closenessThreshold = 0.05
-
-        # Starting state (all LEDs turned off)
-        self.currState = [0,0,0,0]
-
-    def reset(self):
-        self.currState = [0,0,0,0]
-        
-    # The controller app receives an action and updates the state accordingly
-    def sendAction(self,action):
-
-        # Action is a tuple of LED index and delta sign (more/less intensity)
-        idx,sign = action
-
-        # Controller knows about the increment magnitude
-        delta = sign * self.deltas[idx]
-
-        # The system reaches a new state
-        self.currState = getNextState(self.currState,idx,delta)
-
-    def recvReward(self):
-        return isCloseToTarget(self.currState,self.targetSum,self.closenessThreshold)
-        
-    def recvState(self):
-        return self.currState
-        
         
 class ValueFunction(object):
     
@@ -88,21 +32,21 @@ class ValueFunction(object):
     def get(self,state,action):
         return self.Q.get( encode(state,action) ,self.eps )
         
-    def update(self, state, action, reward, alpha = 0.1):
-        h = encode(state,action)
-        self.Q[h] = (1 - alpha) * self.Q.get(h,self.eps) + alpha * reward 
+    def update(self, replayMemory, alpha, log = None):
+        
+        n = len(replayMemory)
+        reward = 1.0 / ( 1 + n )
+        lastState = 4 * [None]
 
+        for state,action in replayMemory:
 
-def updateValue(value,replayMemory,alpha, log = None):
-    
-    alpha = 0.3
-    n = len(replayMemory)
-    reward = 1.0 / ( 1 + len(replayMemory) )
-    lastState = [-1,-1,-1,-1]
-    for state,action in replayMemory:
-        if state != lastState:
-            value.update(state, action, reward, alpha = alpha)
-            lastState = copy.deepcopy(state)
+            if state != lastState:
 
-    if log:
-        log.write('\t'.join(map(str,[n,reward])) + '\n')
+                h = encode(state,action)
+                
+                self.Q[h] = (1 - alpha) * self.Q.get(h,self.eps) + alpha * reward 
+
+                lastState = copy.deepcopy(state)
+
+        if log:
+            log.write('\t'.join(map(str,[n,reward])) + '\n')
