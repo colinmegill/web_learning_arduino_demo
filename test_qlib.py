2f1a72a91855c2114cad6c31dad3a099aa9d103f
terkkila <timo.erkkila@gmail.com>
2014-01-26 09:43:45 -0800

    Updated unit tests

--- test_qlib.py
+++ test_qlib.py
@@ -1,21 +1,38 @@
 #!/usr/bin/python
 
 import qlib
+import numpy as np
+
+def isClose(x,y):
+    return np.abs(x-y) < 1e-10
 
 def test_ValueFunction():
-
+    
     value = qlib.ValueFunction()
-
-    replayMemory = [( [0,0,0,0] , (1,1) ),
-                    ( [0,1,0,0] , (0,1) )]
+    
+    stateActionPair1 = ([0,0,0,0], (1,1) )
+    stateActionPair2 = ([0,1,0,0], (0,1) )
+    
+    replayMemory = [stateActionPair1,stateActionPair2]
 
     assert len(value.Q.keys()) == 0
 
-    assert value.get([0,0,0,0],(1,1)) == value.eps
-
+    state1,action1 = stateActionPair1
+    state2,action2 = stateActionPair2
+    
     alpha = 0.3
     
     value.update(replayMemory,alpha)
+
+    assert isClose( value.get(state1,action1) , (1-alpha) * value.eps + alpha / (1 + 2) )
+    assert isClose( value.get(state2,action2) , (1-alpha) * value.eps + alpha / (1 + 2) )
+
+
+
+
+
+
+
 
 
 
